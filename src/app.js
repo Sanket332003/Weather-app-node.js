ec2cd0b9e323e8d0710524c5a913c21e473400d4
Sanket Beladiya <107698389+Sanket332003@users.noreply.github.com>
2023-01-03 15:37:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const forecast = require('./utils/forecast');
 const path = require('path')
 const  express = require('express')
 const hbs = require('hbs')
-
+const port = process.env.PORT || 3000
 const app = express()
 // Define a path express connfige
 const   publicDirectoryPath = path.join(__dirname,'../public')
@@ -103,6 +103,6 @@ app.get('/weather',(req,res) => {
     
     })
 })
-app.listen(3000,() => {
-    console.log(' Server is up on Port 3000')
+app.listen(port,() => {
+    console.log(' Server is up on Port '+port)
 })
